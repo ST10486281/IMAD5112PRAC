baf820af23d97bca2255ca438e9e40572ac933d9
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-06-19 12:46:46 +0200

    SAtarted tests

---
+++ app/src/androidTest/java/com/example/imad5112prac/DetailViewScreenTest.kt
@@ -0,0 +1,10 @@
+package com.example.imad5112prac
+
+import org.junit.jupiter.api.Assertions.*
+
+class DetailViewScreenTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    fun setUp() {
+    }
+}
---
+++ app/src/androidTest/java/com/example/imad5112prac/MainActivityTest.kt
@@ -0,0 +1,10 @@
+package com.example.imad5112prac
+
+import org.junit.jupiter.api.Assertions.*
+
+class MainActivityTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    fun setUp() {
+    }
+}
