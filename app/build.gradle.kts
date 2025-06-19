2c3806257652c4e8878a20fbd541abaf17ecf148
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-06-19 09:56:40 +0200

    Fix glitch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.imad5112prac"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.imad5112prac"
