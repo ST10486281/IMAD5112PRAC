9d9e8fa90f22c1ca23579caf33e7799b59824857
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-06-19 10:07:56 +0200

    Start of main screen

--- app/src/main/java/com/example/imad5112prac/MainActivity.kt
+++ app/src/main/java/com/example/imad5112prac/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.imad5112prac
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val add_to_playlist = findViewById<Button>(R.id.add_to_playlist)
+        val view_details = findViewById<Button>(R.id.view_details)
+        val exit_app = findViewById<Button>(R.id.exit_app)
+
+
     }
 }
