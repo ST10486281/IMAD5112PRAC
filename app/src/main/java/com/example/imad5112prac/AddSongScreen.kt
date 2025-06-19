3d581baa07d87cd98da3a689347c61a9f49e4743
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-06-19 11:13:08 +0200

    Start of add song screen I assume this is needed

---
+++ app/src/main/java/com/example/imad5112prac/AddSongScreen.kt
@@ -0,0 +1,20 @@
+package com.example.imad5112prac
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class AddSongScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_song_screen)
+
+        val song_title = findViewById<EditText>(R.id.song_title)
+        val artist_name = findViewById<EditText>(R.id.artist_name)
+        val rating_1_to_5 = findViewById<EditText>(R.id.rating_1_to_5)
+        val comments = findViewById<EditText>(R.id.comments)
+
+        val add_song = findViewById<Button>(R.id.add_song)
+    }
+}
